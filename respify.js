d877af663e637e08323525fd9bb8ab370864cc6f
Nils Breunese <nils@breun.nl>
2015-03-26 17:15:02 +0100

    Quote URL
    closes #1

--- respify.js
+++ respify.js
@@ -102,7 +102,7 @@
         if( ! this.settings.background ) {
             this.setImage( encodeURI( this.currentMatch.src ), this.$el.data( 'alt' ) );
         } else {
-            this.$el.css( 'background-image', 'url(' + encodeURI( this.currentMatch.src ) + ')' );
+            this.$el.css( 'background-image', 'url("' + encodeURI( this.currentMatch.src ) + '")' );
         }
     };
 
